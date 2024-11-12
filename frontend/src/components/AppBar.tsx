8882b45130609c3cf4e2ecf06b803762473fa483
Pahul Gogna <135852041+PahulGogna@users.noreply.github.com>
2024-11-13 00:37:37 +0530

    adding logout button

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,12 +1,41 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import { Avatar } from './BlogCard'
 import { useRecoilValueLoadable } from 'recoil'
 import { userAtom } from '../store/atom/atoms'
 import { Link } from 'react-router-dom'
 
+
+function UserSettings({name, email} : {name: string, email: string}) {
+    return (
+        <div className='bg-slate-300/95 border w-[150px] h-[140px] absolute right-8 mt-14 z-10 rounded flex flex-col border-black'>
+            <div className='border-b border-black h-10 flex flex-col justify-center'>
+                <div className='flex justify-center'>
+                    {name.length < 13 ? name : name.substring(0, 13) + '...'}
+                </div>
+            </div>
+            <div className='border-b border-black h-10 flex flex-col justify-center'>
+                <div className='flex justify-center'>
+                    {email.length < 13 ? email : email.substring(0, 13) + '...'}
+                </div>
+            </div>
+            <div className='h-10 flex flex-col justify-center'>
+                <div className='mt-5 flex justify-center'>
+                    <button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm py-1.5 w-1/2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" onClick={() => {
+                        localStorage.clear()
+                        window.location.href = '/'
+                    }}>Logout</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
 function AppBar() {
     
     const user = useRecoilValueLoadable(userAtom)
+
+    const [userWindow, setUserWindow] = useState(false)
 
     if(user.state == "loading"){
         return (
@@ -27,26 +56,35 @@ function AppBar() {
         } = user.contents
         
           return (
-            <div className='flex justify-between mt-0 w-full h-16 bg-slate-200'>
-                <Link to={"/"} className='flex justify-center flex-col pl-5 font-semibold text-xl cursor-pointer'>
-                        Medium
-                </Link>
-                {userData.id && 
-                    <div className='flex justify-right pr-5'>
-                        <div className='flex justify-center flex-col p-4'>
-                            <Link to={"/blogs/create"} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-1.5 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Create</Link>
+            <div className='bg-slate-200 w-full'>
+                <div className='flex justify-between mt-0 w-full h-16 bg-slate-200'>
+                    <Link to={"/"} className='flex justify-center flex-col pl-5 font-semibold text-xl cursor-pointer'>
+                            Medium
+                    </Link>
+                    {userData.id && 
+                        <div className='flex justify-right pr-5'>
+                            <div className='flex justify-center flex-col p-4'>
+                                <Link to={"/blogs/create"} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-1.5 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Create</Link>
+                            </div>
+                            <div className='flex justify-center flex-col'>
+                                <Suspense fallback={<Avatar name='.'/>}>
+                                <div className='cursor-pointer' onClick={() => {
+                                    setUserWindow(c => !c)
+                                }}>
+                                    <Avatar name={userData.name ? userData.name : userData.email} size='9'/>
+                                </div>
+                                </Suspense>
+                            </div>
                         </div>
-                        <div className='flex justify-center flex-col'>
-                            <Suspense fallback={<Avatar name='.'/>}>
-                                <Avatar name={userData.name ? userData.name : userData.email} size='9'/>
-                            </Suspense>
-                        </div>
-                    </div>
-                }
+                    }
+                {userWindow ? <UserSettings name={userData.name} email={userData.email}/> : null}
+                </div>
+
+
             </div>
           )
-    }
-
+        }
+        
 }
 
 export default AppBar
